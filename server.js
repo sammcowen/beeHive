96dae6bafd8a0521895df67f36bc4005e1465ace
HBK <sammcowen@gmail.com>
2022-03-10 17:13:25 -0800

    created prompt for menu. created .then and switch statement

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ const express = require('express');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const { restoreDefaultPrompts } = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // connect to datatbase
 const db = mysql.createConnection(
     {
-        host:'localhost',
-        user:'root',
-        password:'password',
-        database:'workplace'
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'workplace'
     },
     console.log('you are connected to the workplace  database')
 )
-function init() {
-    inquirer.prompt({
-        
+function menu() {
+    inquirer
+        .prompt({
+
             type: 'list',
-            name: 'menu',
-            message: "choose one of the following options",
             choices: ['View All DEPARTMENTS',
                 'View All ROLES',
                 'View All EMPLOYEES',
@@ -33,12 +33,41 @@ function init() {
                 'ADD an EMPLOYEE',
                 'UPDATE an EMPLOYEE ROLE'
 
-                ]
-        }
-    )}; 
 
-init();
+            ],
+            name: 'menu',
+            message: "choose one of the following options",
 
-app.listen(PORT, () => {
-    console.log(`you are now on port ${PORT}`);
-});
+
+        })
+        .then(function (answers) {
+            console.log('You chose' + answers.option);
+            switch (answers.option) {
+                case 'View All DEPARTMENTS':
+                    viewDepartments();
+                    break;
+                case 'View All ROLES':
+                    viewRoles();
+                    break;
+                case 'ADD a DEPARTMENT ':
+                    addDepartment();
+                    break;
+                case 'ADD a ROLE':
+                    addRole();
+                    break;
+                case 'ADD an EMPLOYEE':
+                    addEmployee();
+                    break;
+                    case 'UPDATE an EMPLOYEE ROLE':
+                        updateEmployee();
+                        break;
+                        default:exit();
+
+            }
+        });
+    }
+    menu();
+
+    app.listen(PORT, () => {
+        console.log(`you are now on port ${PORT}`);
+    });
